7d4739e3859ad7416d65053692cc0f5744ac6329
Vinícius Oliveira Moreira <187030248+SampaioDevHub@users.noreply.github.com>
2026-01-04 22:43:16 -0300

    init project

---
+++ src/main/java/org/tws/javalangchain4j/JavaLangChain4jApplication.java
@@ -0,0 +1,13 @@
+package org.tws.javalangchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLangChain4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLangChain4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tws/javalangchain4j/JavaLangChain4jApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tws.javalangchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLangChain4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
